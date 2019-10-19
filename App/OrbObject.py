43681da86e17516ab1e8df3981ba35897cda2079
ostapdyg <dyhdalovych@ucu.edu.ua>
2019-10-19 15:20:04 +0300

    Some funcs

--- App/OrbObject.py
+++ App/OrbObject.py
@@ -1,25 +1,28 @@
-from main import *
-
+from __init__ import *
 class OrbitalObject(pygame.sprite.Sprite):
     def __init__(self, x, y, vx, vy):
         super(OrbitalObject, self).__init__()
         self.surf = pygame.Surface((10, 10))
         self.surf.fill((255,255,255))
-        self.rect = self.surf.pygame.Surface.get_rect(center = (x + CENTER_X, y + CENTER_Y))
+        self.rect = self.surf.get_rect(center = (x + CENTER_X, y + CENTER_Y))
         self.vx = vx
         self.vy = vy
+        self.x = x
+        self.y = y
 
     def get_x(self):
-        return self.rect.x - CENTER_X
+        return self.x
 
     def get_y(self):
         
-        return self.rect.y - CENTER_Y
+        return self.y
         
     def orbit(self, timestep):
-        self.rect.move_ip(round(self.vx*timestep), round(self.vy*timestep))
-        self.vx += -(self.rect.x / (self.rect.x**2 + self.rect.y**2)**.5)*timestep
-        self.vx += -(self.rect.y / (self.rect.x**2 + self.rect.y**2)**.5)*timestep
+        self.x += self.vx*timestep
+        self.y += self.vy*timestep
+        self.rect.move_ip(round(self.x + CENTER_X)-self.rect.x, round(self.y + CENTER_Y)-self.rect.y)
+        self.vx += -GRAV_CONST*(self.get_x() / (self.get_x()**2 + self.get_y()**2)**.5)*timestep
+        self.vy += -GRAV_CONST*(self.get_y() /  (self.get_x()**2 + self.get_y()**2)**.5)*timestep
 
     def draw(self, ):
         pass
--- App/main.py
+++ App/main.py
@@ -1,5 +1,5 @@
-import pygame
-from OrbObject import OrbObject
+from __init__ import *
+from OrbObject import OrbitalObject
 
 pygame.init()
 
@@ -7,22 +7,24 @@ SCREEN_WIDTH = 800
 
 SCREEN_HEIGHT = 00
 
-CENTER_X = 400
-CENTER_Y = 400
+CENTER_X = 700
+CENTER_Y = 700
 
 
-screen = pygame.display.set_mode([500, 500])
-scrap = OrbObject(0, 10, 100, 0)
+screen = pygame.display.set_mode([1000, 1000])
+scrap = OrbitalObject(0, 10, 50, 0)
 screen.fill((0,0,0))
+
+pygame.draw.circle(screen, (0, 0, 255), (CENTER_X-50, CENTER_Y), 50)
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    scrap.orbit(.01)
+    scrap.orbit(.1)
     screen.blit(scrap.surf, scrap.rect)
-    screen.flip()
+    pygame.display.flip()
     
 
 
