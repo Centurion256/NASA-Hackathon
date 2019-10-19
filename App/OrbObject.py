cc6fb4b4e8e6600453ff6146b8eb56a9f83557f2
Centurion256 <vkuchynskyy@gmail.com>
2019-10-19 14:30:58 +0300

    Added physics, pygame support

--- App/OrbObject.py
+++ App/OrbObject.py
@@ -1,4 +1,25 @@
-class OrbObject:
-    def __init__(self, position, velocity):
-        self.position = position
-        self.velocity = velocity
+from main import *
+
+class OrbitalObject(pygame.sprite.Sprite):
+    def __init__(self, x, y, vx, vy):
+        super(OrbitalObject, self).__init__()
+        self.surf = pygame.Surface((10, 10))
+        self.surf.fill((255,255,255))
+        self.rect = self.surf.pygame.Surface.get_rect(center = (x + CENTER_X, y + CENTER_Y))
+        self.vx = vx
+        self.vy = vy
+
+    def get_x(self):
+        return self.rect.x - CENTER_X
+
+    def get_y(self):
+        
+        return self.rect.y - CENTER_Y
+        
+    def orbit(self, timestep):
+        self.rect.move_ip(round(self.vx*timestep), round(self.vy*timestep))
+        self.vx += -(self.rect.x / (self.rect.x**2 + self.rect.y**2)**.5)*timestep
+        self.vx += -(self.rect.y / (self.rect.x**2 + self.rect.y**2)**.5)*timestep
+
+    def draw(self, ):
+        pass
--- App/main.py
+++ App/main.py
@@ -1,22 +1,29 @@
 import pygame
+from OrbObject import OrbObject
 
 pygame.init()
 
 SCREEN_WIDTH = 800
 
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = 00
+
+CENTER_X = 400
+CENTER_Y = 400
 
 
 screen = pygame.display.set_mode([500, 500])
+scrap = OrbObject(0, 10, 100, 0)
+screen.fill((0,0,0))
+running = True
 
-
-while True:
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-    screen.fill((100,100,100))
-    pygame.display.flip()
-
+            running = False
+    scrap.orbit(.01)
+    screen.blit(scrap.surf, scrap.rect)
+    screen.flip()
+    
 
 
 
